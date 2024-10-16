a90d9aa1f39bfea3c772fb1b465dc5ed57c27588
Daniel Frey <dmfrey@gmail.com>
2024-10-16 19:38:38 -0400

    add condition to task config

--- src/main/java/com/broadcom/springconsulting/batch_demo/config/TaskConfiguration.java
+++ src/main/java/com/broadcom/springconsulting/batch_demo/config/TaskConfiguration.java
@@ -1,10 +1,13 @@
 package com.broadcom.springconsulting.batch_demo.config;
 
+import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.cloud.task.configuration.EnableTask;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableTask
+@ConditionalOnBean( SpringLiquibase.class )
 public class TaskConfiguration {
 
 }
--- src/test/java/com/broadcom/springconsulting/batch_demo/TestcontainersConfiguration.java
+++ src/test/java/com/broadcom/springconsulting/batch_demo/TestcontainersConfiguration.java
@@ -3,10 +3,16 @@ package com.broadcom.springconsulting.batch_demo;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.TestPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration( proxyBeanMethods = false )
+@TestPropertySource(
+		properties = {
+				"spring.docker.compose.enabled=false"
+		}
+)
 class TestcontainersConfiguration {
 
 	@Bean
