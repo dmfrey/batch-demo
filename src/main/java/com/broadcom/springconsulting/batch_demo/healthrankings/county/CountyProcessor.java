cfd65f3782ff9cd6be0f8d970c52df30e2c2b29f
Daniel Frey <dmfrey@gmail.com>
2024-10-26 20:12:37 -0400

    update count tests for skips

--- src/main/java/com/broadcom/springconsulting/batch_demo/healthrankings/county/CountyProcessor.java
+++ src/main/java/com/broadcom/springconsulting/batch_demo/healthrankings/county/CountyProcessor.java
@@ -1,14 +1,24 @@
 package com.broadcom.springconsulting.batch_demo.healthrankings.county;
 
+import com.broadcom.springconsulting.batch_demo.healthrankings.county.exception.CountyIdAlreadyExistsCountyProcessorException;
+import com.broadcom.springconsulting.batch_demo.healthrankings.county.exception.CountyIdRequiredCountyProcessorException;
 import com.broadcom.springconsulting.batch_demo.input.InputRow;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.item.ItemProcessor;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.lang.NonNull;
 
 public class CountyProcessor implements ItemProcessor<InputRow, County> {
 
     private static final Logger log = LoggerFactory.getLogger( CountyProcessor.class );
+
+    private final JdbcTemplate jdbcTemplate;
+    public CountyProcessor( final JdbcTemplate jdbcTemplate ) {
+
+        this.jdbcTemplate = jdbcTemplate;
+
+    }
 
     @Override
     public County process( @NonNull InputRow input ) throws Exception {
@@ -17,7 +27,15 @@ public class CountyProcessor implements ItemProcessor<InputRow, County> {
         if( null == input.countyCode() ) {
             log.error( "process : countyCode is null, skipping" );
 
-            return null;
+            throw new CountyIdRequiredCountyProcessorException();
+        }
+
+        var existing =
+                this.jdbcTemplate.queryForObject(
+                        "SELECT count(county_code) FROM county WHERE county_code = ?", Integer.class, input.countyCode() );
+        if( null != existing && !existing.equals( 0 ) ) {
+
+            throw new CountyIdAlreadyExistsCountyProcessorException();
         }
 
         var county = new County(
---
+++ src/main/java/com/broadcom/springconsulting/batch_demo/healthrankings/county/exception/CountyProcessorException.java
@@ -0,0 +1,4 @@
+package com.broadcom.springconsulting.batch_demo.healthrankings.county.exception;
+
+public abstract class CountyProcessorException extends Exception {
+}
